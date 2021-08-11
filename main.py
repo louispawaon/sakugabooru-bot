5b0d6adfb89cf7c44713a6b86bcdbf5b6af9059a
tremor6916 <lpawaon@gmail.com>
2021-08-11 09:30:24 +0800

    while true loop testing

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ from pybooru import Moebooru
 artistlist=[]
 posturl='https://www.sakugabooru.com/post/show/'
 client = Moebooru(site_url='https://www.sakugabooru.com')
-array=client.artist_list(order="date")
+array = client.artist_list(order="date")
 files = client.post_list(tags="order:random")
 for x in array:
     artistlist.append((x['name']))
 
+#while (True):
 try:
     choice = random.choice(artistlist)
     print(choice)
     files = client.post_list(tags="{}".format(choice))
-    filechoice = random.choice(files)
-    print("File: {0}".format(filechoice['file_url']))
-    print(posturl+"{0}".format(filechoice['id']))
+    filechoice = random.choice(files) 
+    print("File: {0}".format(filechoice['file_url'])) #File Item Print
+    print(posturl+"{0}".format(filechoice['id'])) #URL Print
 except:
     pass
