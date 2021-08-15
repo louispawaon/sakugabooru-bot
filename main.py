a67316fb69b221ae99e3f5c9a0c6e38a73fd469d
tremor6916 <lpawaon@gmail.com>
2021-08-15 16:33:45 +0800

    animatorname and number of posts implementation

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import tweepy
 import random
 import time
 import os
+import bs4
+from bs4 import BeautifulSoup
 from pybooru import Moebooru
 
-artistlist=[]
 siteurl='https://www.sakugabooru.com/post/show/'
 client = Moebooru(site_url='https://www.sakugabooru.com')#Might change
 files = client.post_list(tags="order:random")
@@ -20,26 +21,28 @@ access_token_secret=lines[10].rstrip()
 def main():
     #while (True):
     try:
-        choice = random.choice(artistlist) #Artist Name
-        print(choice)
-        files = client.post_list(tags="{}".format(choice))
-        filechoice = random.choice(files) 
-        boorurl=filechoice['file_url'] #File Item
+        files = client.post_list(tags="order:random")#Random Post 
+        choice = random.choice(files) #Select 1 Random Post from Query
+        boorurl=choice['file_url'] #File URL
+        tags = choice['tags'] #Post Tags
         verdict=filetypechecker(boorurl)
-        #print(filechoice)
+        posturl = siteurl+"{0}".format(choice['id'])#URL Print
         print(verdict)
-        head = requests.head(boorurl)
+        #head = requests.head(boorurl)
         #For filesize checking 
         '''
         if(filesizechecker(head)): 
             pass 
         '''
+        animatorname=artistgrabber(posturl)
+        '''
         data = requests.get(boorurl)
         with open("C:/Users/Admin/Documents/PersonalFiles/Repositories/sakugabooru-video-files/{}".format(filechoice['id'])+".mp4",'wb') as file: #Customize
             file.write(data.content)
         time.sleep(5)
-        mediapost()
-        posturl = siteurl+"{0}".format(filechoice['id'])#URL Print
+        '''
+        testpost(animatorname,tags,posturl)
+        #mediapost()
     except:
         pass
 
@@ -51,6 +54,13 @@ def filesizechecker(head):
     if content_length and content_length > 3072:
         return False
 '''
+def artistgrabber(posturl):
+    r =requests.get(posturl)
+    soup = bs4.BeautifulSoup(r.text,'lxml')
+    for div in soup.find_all(class_="sidebar"):
+        artist=div.find(class_="tag-type-artist").text
+    artistname=(artist.strip("? "))
+    return artistname
 
 def filetypechecker(boorurl):
     if boorurl.find('/'):
@@ -80,6 +90,21 @@ def mediapost():
     except Exception as e:
         print(e)
        
+def testpost(animatorname,tags,posturl):
+    try:
+        auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+        auth.set_access_token(access_token,access_token_secret)
+        api = tweepy.API(auth)
+        #Looking into how to upload properly using tweepy
+        #os.chdir('C:/Users/Admin/Documents/PersonalFiles/Repositories/sakugabooru-video-files')
+    except Exception as e:
+        print (e)
+
+    try:
+        api.update_status(status="Animator Name: {}Tags: {}Post URL: {} TestTweet".format(animatorname,tags,posturl))
+    except Exception as e:
+        print (e)
+
 if __name__ == '__main__':
     main()
 
