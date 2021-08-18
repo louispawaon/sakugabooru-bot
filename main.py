5c928632d15089eb3f63c69dc96277e0326a5282
tremor6916 <lpawaon@gmail.com>
2021-08-18 21:53:59 +0800

    ready for implementation

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from pybooru import Moebooru
 siteurl='https://www.sakugabooru.com/post/show/'
 client = Moebooru(site_url='https://www.sakugabooru.com')
 files = client.post_list(tags="order:random")
-    
-api_keys = open("token.txt")
+     
+api_keys = open("token.txt") #Create your own token.txt file with your API Keys from Twitter
 lines = api_keys.readlines()
 consumer_key = lines[1].rstrip()
 consumer_secret= lines[4].rstrip()
@@ -27,43 +27,33 @@ def main():
             tags = choice['tags'] #Post Tags
             verdict=filetypechecker(boorurl) #Checker if .mp4 file or not
             if(verdict):
-                posturl = siteurl+"{0}".format(choice['id'])#URL Print
-                #head = requests.head(boorurl)
-                #For filesize checking 
-                '''
-                if(filesizechecker(head)): 
-                    pass 
-                '''
+                posturl = siteurl+"{0}".format(choice['id']) #POST URL from SakugaBooru
+
                 animatorname=artistgrabber(posturl)
                 time.sleep(5)
                 
                 data = requests.get(boorurl,headers={'user-agent': 'Mozilla/5.0'},timeout=5)
                 print("data:",data.status_code)
-                with open("C:/Users/Admin/Documents/PersonalFiles/Repositories/sakugabooru-video-files/{}".format(choice['id'])+".mp4",'wb') as file: #Customize
+                with open("C:/Users/Admin/Documents/PersonalFiles/Repositories/sakugabooru-video-files/{}".format(choice['id'])+".mp4",'wb') as file: #Customize Directory
                     file.write(data.content)
                 
                 params="Animator Name: {}\nTags: {}\nPost URL: {}\n".format(animatorname,tags,posturl)
 
                 time.sleep(5)
                 mediapost(params)
+
         except Exception as e:
             print("Main() Error:",e)
            
-#Filesize Checker
-'''
-def filesizechecker(head):
-    header = head.headers
-    content_length = header.get('content_length')
-    if content_length and content_length > 3072:
-        return False
-'''
 def artistgrabber(posturl):
-    r =requests.get(posturl,headers={'user-agent': 'Mozilla/5.0'}, timeout=5)
+    r = requests.get(posturl,headers={'user-agent': 'Mozilla/5.0'}, timeout=5)
     print("artistgrabber:",r.status_code)
     soup = bs4.BeautifulSoup(r.text,'lxml')
+
     for div in soup.find_all(class_="sidebar"):
         artist=div.find(class_="tag-type-artist").text
     artistname=(artist.strip("? "))
+
     return artistname
 
 def filetypechecker(boorurl):
@@ -78,43 +68,30 @@ def mediapost(params):
         auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
         auth.set_access_token(access_token,access_token_secret)
         api = tweepy.API(auth)
+
     except Exception as e:
         print (e)
        
     try:
         file_path=[]
-        directory_name='C:/Users/Admin/Documents/PersonalFiles/Repositories/sakugabooru-video-files'
+        directory_name='C:/Users/Admin/Documents/PersonalFiles/Repositories/sakugabooru-video-files' #Customize Directory
         media_list=filter(lambda x: os.path.isfile(os.path.join(directory_name,x)),os.listdir(directory_name))
         media_list=sorted(media_list,key=lambda x: os.path.getmtime(os.path.join(directory_name,x)),reverse=True)
+
         for media in media_list:
             file_path.append(os.path.join(directory_name,media))       
         media=file_path[0]
+
         print(media)
         upload_media=api.media_upload(media, media_category='tweet_video')
         api.update_status(status=params, media_ids=[upload_media.media_id_string])
+
     except Exception as e:
         print("Mediapost() Error:",e)
+        
 schedule.every(45).minutes.do(main)
-#schedule.every(45).seconds.do(main)
-'''    
-def testpost(params):
-    try:
-        auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-        auth.set_access_token(access_token,access_token_secret)
-        api = tweepy.API(auth)
-    except Exception as e:
-        print (e)
 
-    try:
-        api.update_status(status=params)
-    except Exception as e:
-        print (e)
-'''
 if __name__ == '__main__':
     while True:
         schedule.run_pending()
         time.sleep(1)
-
-#Change methodology for upload of video
-#Limit File Size
-#attempt ug 2 min cooldown
